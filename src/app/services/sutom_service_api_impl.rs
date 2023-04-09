b2411ba38843b95ca6b969abf32d75429352817c
Pierre Paquin <p.paquin1016@laposte.net>
2023-04-09 10:42:50 +0200

    feat: ajout parties

--- src/app/services/sutom_service_api_impl.rs
+++ src/app/services/sutom_service_api_impl.rs
@@ -4,7 +4,7 @@ use crate::models::commands::create_player_command::CreatePlayer;
 use futures::TryFutureExt;
 use crate::core::entities::party::Party;
 
-pub async fn player_exist(url: String, name: String) -> Result<bool, String> {
+pub async fn player_exist(url: &String, name: &String) -> Result<bool, String> {
     reqwest::get(format!("{}/players", url))
         .and_then(|response| {
             let body = response.json::<Vec<Player>>();
@@ -15,13 +15,13 @@ pub async fn player_exist(url: String, name: String) -> Result<bool, String> {
             players
                 .into_iter()
                 .any(|player| {
-                    player.name == name
+                    player.name == name.clone()
                 })
         })
         .map_err(|_| "une erreur est survenu".into())
 }
 
-pub async fn add_party(url: String, party: Party, name: String) -> Result<u16, String> {
+pub async fn add_party(url: &String, party: &Party, name: &String) -> Result<u16, String> {
     reqwest::Client::new()
         .put(format!("{}/players/commands/add-party/{}", url, name))
         .json(&party)
@@ -35,10 +35,10 @@ pub async fn create_player(name: &String) -> Result<(), String> {
     let contents = include_str!("../../../config.toml");
     let config: Value = toml::from_str(contents).expect("Could not parse TOML");
     let url = config["api"]["SUTOM_API_KEY"].as_str().expect("url chargement impossible");
-    create_player_from_url(url, name).await
+    create_player_from_url(&url.to_string(), name).await
 }
 
-pub async fn create_player_from_url(url: &str, name: &String) -> Result<(), String> {
+pub async fn create_player_from_url(url: &String, name: &String) -> Result<(), String> {
     reqwest::Client::new()
         .post(format!("{}/players/commands/create", url))
         .json(&CreatePlayer::new(name.clone()))
@@ -46,4 +46,11 @@ pub async fn create_player_from_url(url: &str, name: &String) -> Result<(), Stri
         .await
         .map(|_| ())
         .map_err(|err| err.to_string())
+}
+
+pub fn get_url() -> String {
+    let contents = include_str!("../../../config.toml");
+    let config: Value = toml::from_str(contents).expect("Could not parse TOML");
+    let url = config["api"]["SUTOM_API_KEY"].as_str().expect("url chargement impossible");
+    url.to_string()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::string::ToString;
 
 use dioxus::prelude::*;
+use futures::TryFutureExt;
 use log::{info, LevelFilter};
 
 use crate::app::components::hello_world_components::hello_world_component;
 use crate::app::services::party_handler_impl::PartyHandlerImpl;
-use crate::app::services::sutom_service_api_impl::create_player;
+use crate::app::services::sutom_service_api_impl::{add_party, create_player, create_player_from_url, get_url, player_exist};
+use crate::core::entities::party::Party;
 use crate::core::services::party_handler::PartyHandler;
 
 mod app;
@@ -18,6 +20,34 @@ fn main() {
     dioxus_web::launch(app);
 }
 
+async fn traitement(exist: bool, url: &String, party: &Party, name_player_content: &String) -> Result<String, String> {
+    if exist.clone() {
+        add_party(url, party, name_player_content)
+            .await
+            .map(|res| {
+                if res >= 400 {
+                    String::from("vous avez déjà joué aujourd'hui")
+                } else {
+                    String::from("la partie a bien ete ajoutée 😘")
+                }
+            })
+    } else {
+        create_player_from_url(url, name_player_content)
+            .and_then(|_| async move {
+                add_party(url, party, name_player_content)
+                    .and_then(|res| async move {
+                        if res >= 400 {
+                            Ok(String::from("vous avez déjà joué aujourd'hui"))
+                        } else {
+                            Ok(String::from("la partie a bien ete ajoutée 😘"))
+                        }
+                    })
+                    .await
+            })
+            .await
+    }
+}
+
 fn app(cx: Scope) -> Element {
 
     let name_player: &UseState<String> = use_state(cx, || "bob".to_string());
@@ -26,21 +56,21 @@ fn app(cx: Scope) -> Element {
     let add_party_player = move |_| {
         let name_player_content = name_player.get().clone();
         let partie_content = partie_state.get().clone();
+        let url = get_url();
+        let party = PartyHandlerImpl::handle_message(&partie_content).expect("error parsing");
 
         cx.spawn({
             async move {
 
-                let party = PartyHandlerImpl::handle_message(&partie_content).expect("error parsing");
-
-                info!("{}", partie_content);
-
-                create_player(&name_player_content)
+                player_exist(&url, &name_player_content)
+                    .and_then(|exist| traitement(exist, &url, &party, &name_player_content))
                     .await
-                    .map(|_| info!("called"))
-                    .map_err(|err| {
-                        err
-                    })
-                    .expect("communication au service impossible");
+                    .expect("lors de l'ajout de partie");
+
+                // match player_exist(&url, &name_player_content).await {
+                //     Ok(exist) => traitement(exist, &url, &party, &name_player_content).await,
+                //     Err(err) => Err(err)
+                // }.expect("lors de l'ajout de partie");
             }
         });
     };
